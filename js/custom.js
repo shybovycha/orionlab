f5be453348b67aae4ab0d8e66182619b63c9a803
Artem Shoobovych <shybovycha@gmail.com>
2013-08-04 23:11:45 +0300

    Created statics page with the design proposal.

---
+++ js/custom.js
@@ -0,0 +1,31 @@
+function setup_map() {
+    var map = L.map('contact-map').setView([50.489511, 30.472686], 18);
+
+    L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
+    }).addTo(map);
+
+    L.marker([50.489511, 30.472686]).addTo(map).bindPopup($('#contact-map-marker-popup-html').html()).openPopup();
+}
+
+function setup_gallery() {
+	$(".fancybox-thumb").fancybox({
+		prevEffect	: 'swing',
+		nextEffect	: 'swing',
+		helpers	: {
+			title	: {
+				type: 'outside'
+			},
+			thumbs	: {
+				width	: 50,
+				height	: 50
+			}
+		}
+	});
+}
+
+$(document).ready(function() {
+    setup_map();
+    setup_gallery();
+});
